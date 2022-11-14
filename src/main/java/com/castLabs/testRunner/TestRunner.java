3b06d0093f818ffb8da99027f676dceeef9672cd
pushpanjali93 <109785560+pushpanjali93@users.noreply.github.com>
2022-11-14 21:08:50 +0530

    Add files via upload

--- src/main/java/com/castLabs/testRunner/TestRunner.java
+++ src/main/java/com/castLabs/testRunner/TestRunner.java
@@ -5,6 +5,8 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
+import java.util.stream.Stream;
+
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src\\main\\java\\com\\castLabs\\features",
@@ -16,4 +18,15 @@ import org.junit.runner.RunWith;
         }
 )
 public class TestRunner {
+        private static String[] defaultOptions = {
+                "src\\main\\java\\com\\castLabs\\features",
+                "--glue", "com.castLabs.stepDefinitions",
+                "--plugin", "pretty",
+                "--plugin", "html:test-output"
+        };
+
+        public static void main(String[] args) throws Throwable {
+                Stream<String> cucumberOptions = Stream.concat(Stream.of(defaultOptions), Stream.of(args));
+                io.cucumber.core.cli.Main.main(cucumberOptions.toArray(String[]::new));
+        }
 }
